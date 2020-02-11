8bec21313ac5e5a663877939734f65c2fda16944
Albert <ankahira@gmail.com>
2020-02-11 18:14:22 +0100

    restructure results

--- experiments.old/channel/launch_all_jobs.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Channel Parallelism jobs
-
-qsub -g gaa50004 scripts/alexnet/alexnet_3.sh
-
-qsub -g gaa50004 scripts/vgg/vgg_3.sh
-
-qsub -g gaa50004 scripts/resnet/resnet_3.sh
--- experiments.old/pipeline/launch_all_jobs.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Data Pipeline jobs
-
-qsub -g gaa50004 scripts/alexnet/submit_alexnet_32_4.sh
-qsub -g gaa50004 scripts/alexnet/submit_alexnet_64_4.sh
-qsub -g gaa50004 scripts/alexnet/submit_alexnet_128_4.sh
-
-
